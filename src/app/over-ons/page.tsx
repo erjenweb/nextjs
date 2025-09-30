666f8f95e889513232a4bde18695be132934a4cf
erjenweb <rijnderserjen@gmail.com>
2025-09-30 18:29:54 +0530

    Update page.tsx

--- src/app/over-ons/page.tsx
+++ src/app/over-ons/page.tsx
@@ -233,13 +233,6 @@ function OverOnsPage() {
         </div>
       </section>
       <section className="about-faq">
-      <div className="collapse bg-base-100 border-base-300 border">
-        <input type="checkbox" />
-        <div className="collapse-title font-semibold">How do I create an account?</div>
-        <div className="collapse-content text-sm">
-          Click the "Sign Up" button in the top right corner and follow the registration process.
-        </div>
-      </div>
        </section>
     </>
   )
