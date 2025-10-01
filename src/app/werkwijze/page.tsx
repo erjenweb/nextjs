8c52c2a64083ccf046b3a17b86c89e3b52847031
erjenweb <rijnderserjen@gmail.com>
2025-10-01 18:49:58 +0530

    Update page.tsx

--- src/app/werkwijze/page.tsx
+++ src/app/werkwijze/page.tsx
@@ -4,238 +4,106 @@ import FAQ from "@/components/page/faq";
 function WerkwijzePage() {
   return (
     <>
-      <section className="about-section">
-        <div className="max-w-7xl mx-auto">
-          <div className="flex">
-            <div className="about-content">
-              <h1 className="ft-48 white-text"><span className="orange-text">over</span> cartelier</h1>
-              <p className="ft-22 white-text">Carteliér is ontstaan uit een jarenlange passie voor auto’s. Wij
-                zijn een RDW erkend, jong dynamisch bedrijf, wat zich
-                richt op in- en verkoop van auto’s die rijk zijn uitgerust aan opties. </p>
-              <a href="#" className="primary-btn orange-bg arrow-btn">Contact opnemen</a>
-            </div>
-            <div className="about-img">
-              <img src="/assets/image/about-img.webp" alt="" />
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="about-section-two">
-        <div className="max-w-7xl mx-auto">
-          <div className="flex items-center">
-            <div className="about-img-two relative">
-              <img src="/assets/image/about-img-2.webp" alt="" />
-              <img src="/assets/image/exclusive-img.webp" alt="" className="text-icon" />
-            </div>
-            <div className="about-content-two">
-              <img src="/assets/image/about-two-icon.svg" alt="" />
-              <h2 className="ft-48 text-white">focus op service,<br /> betrouwbaarheid <span className="orange-text">en
-                een<br />
-                goede prijs</span></h2>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="about-list">
-        <div className="max-w-7xl mx-auto">
-          <div className="flex">
-            <div className="about-list-left">
-              <p>Wij zoeken de parels op vier wielen in héél Europa! Dat hoeft niet alleen in Duitsland te
-                zijn maar kan ook bijvoorbeeld
-                diep in Zuid-Italië zijn. Wij in- en verkopen niet alleen maar verrichten ook gericht
-                zoekwerk voor klanten met de wens
-                naar een specifieke auto met bepaalde e/o gewenste optie(s)</p>
-              <p>Inruilen van uw oude auto is geen enkel probleem. Wilt u langskomen voor een proefrit in één
-                van onze occasions of
-                om  een gerichte zoekopdracht met ons te bespreken? Maak dan een afspraak met ons. Ook kunt
-                u onderstaand formulier
-                invullen met uw naam en telefoonnummer, dan bellen wij u op om alles te bespreken. Een
-                afspraak in de avonduren is ook
-                mogelijk.</p>
-              <p>Bel even voordat u langs wilt komen, dan staat de koffie voor u klaar!</p>
-              <img src="/assets/image/man-and-woman-drinking-coffee-at-restaurant-busin-2025-03-26-12-15-22-utc 1.webp"
-                alt="" />
-            </div>
-            <div className="about-list-right">
-              <ul className="service_list">
-                <li>
-                  <div className="icon">
-                    <img src="/assets/icons/list.svg" alt="" />
-                  </div>
-                  <div className="text">
-                    <span className="title">Eenvoudig proces</span>
-                    <p>Bij Cartelier verloopt het proces van reserveren of aanschaffen van uw droomauto
-                      moeiteloos.</p>
-                  </div>
-                </li>
-                <li>
-                  <div className="icon">
-                    <img src="/assets/icons/clock.svg" alt="" />
-                  </div>
-                  <div className="text">
-                    <span className="title">altijd op tijd</span>
-                    <p>Bij Cartelier verloopt het proces van reserveren of aanschaffen van uw droomauto
-                      moeiteloos.</p>
-                  </div>
-                </li>
-                <li>
-                  <div className="icon">
-                    <img src="/assets/icons/lock.svg" alt="" />
-                  </div>
-                  <div className="text">
-                    <span className="title">Totale bescherming</span>
-                    <p>Bij Cartelier verloopt het proces van reserveren of aanschaffen van uw droomauto
-                      moeiteloos.</p>
-                  </div>
-                </li>
-                <li>
-                  <div className="icon">
-                    <img src="/assets/icons/crown.svg" alt="" />
-                  </div>
-                  <div className="text">
-                    <span className="title">premium service</span>
-                    <p>Bij Cartelier verloopt het proces van reserveren of aanschaffen van uw droomauto
-                      moeiteloos.</p>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="about-vision-section">
-        <div className="max-w-7xl mx-auto">
-          <div className="flex items-center">
-            <div className="vision-content">
-              <h3 className="ft-48 white-text"><span className="orange-text">Onze</span><br /> Visie</h3>
-              <p>Cartelier wil uitgroeien tot een gevestigde naam in Twente voor kwalitatieve autos uit het
-                midden- en hoogsegment. Wij
-                streven ernaar dé partner te zijn voor klanten die zorgeloos hun autowensen willen
-                uitbesteden, én voor liefhebbers die
-                zelf op zoek zijn naar een auto met karakter en kwaliteit. Door transparantie, scherpe
-                prijzen en betrokken service
-                willen we uitblinken in betrouwbaarheid en klanttevredenheid.</p>
-            </div>
-            <div className="mission-content">
-              <h3 className="ft-48 white-text"><span className="orange-text">Onze</span><br /> Missie</h3>
-              <p>Cartelier is gespecialiseerd in het leveren van representatieve auto’s uit het midden- en
-                hoogsegment die nét dat beetje
-                extra bieden. Wij kopen gericht auto’s in die voldoen aan onze hoge kwaliteitsstandaard, én
-                gaan op zoek naar autos op
-                basis van de specifieke wensen van onze klanten. Of het nu gaat om bemiddeling of directe
-                verkoop, wij bieden een
-                zorgeloze aankoopervaring tegen een scherp tarief en met persoonlijke aandacht.</p>
-              <img src="/assets/image/DSC08026 2.webp" alt="" />
-            </div>
-          </div>
-        </div>
-      </section >
-      <section className="contact-section pd-70">
-        <div className="contact-inner">
-          <div className="max-w-7xl mx-auto">
-            <div className="contact-heading flex items-end justify-between">
-              <h3 className="text-[48px] uppercase dark-text"><span className="orange-text">we helpen grAAg</span><br /> met de
-                zoektocht
-              </h3>
-              <p>Heeft u vragen of zoekt u een specifieke auto? Vul het formulier in en ons team neemt
-                persoonlijk
-                contact met u op om u
-                verder te helpen.</p>
-            </div>
-            <div className="flex justify-between">
-              <div className="contact-form">
-                <div className="form-first">
-                  <span className="title">persoonlijke gegevens</span>
-                  <div className="flex justify-between">
-                    <div className="input-field">
-                      <label htmlFor="">Volledige naam</label>
-                      <input type="text" placeholder="Hoe mogen we je noemen" />
+      <section className="werk-section">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between">
+                    <div className="werk-content">
+                        <h1 className="ft-48 white-text"><span className="orange-text">onze</span> werkwijze</h1>
+                        <p className="ft-22 white-text">Bij Cartelier BV bieden wij twee mogelijkheden om u van dienst te
+                            zijn: bemiddeling bij het vinden van uw droomauto óf
+                            directe aankoop uit ons zorgvuldig geselecteerde aanbod. In beide gevallen staat kwaliteit,
+                            transparantie en
+                            persoonlijke aandacht centraal.</p>
+                        <a href="#" className="primary-btn orange-bg arrow-btn">Contact opnemen</a>
                     </div>
-                    <div className="input-field">
-                      <label htmlFor="">E-mailadres</label>
-                      <input type="text" placeholder="vul je e-mailadres in" />
+                    <div className="werk-right">
+                        <img src="/assets/image/contact-img.webp" alt="">
                     </div>
-                    <div className="input-field">
-                      <label htmlFor="">Telefoonnummer</label>
-                      <input type="text" placeholder="Je telefoonnummer" />
-                    </div>
-                  </div>
                 </div>
-                <div className="form-second">
-                  <span className="title">gegevens zoekopdracht</span>
-                  <div className="flex justify-between">
-                    <div className="input-field">
-                      <label htmlFor="">Automerk</label>
-                      <input type="text" placeholder="Vul automerk in" />
-                    </div>
-                    <div className="input-field">
-                      <label htmlFor="">Model</label>
-                      <input type="text" placeholder="Model" />
-                    </div>
-                    <div className="input-field">
-                      <label htmlFor="">Kleur</label>
-                      <input type="text" placeholder="Vul gewenste kleur in" />
-                    </div>
-                    <div className="input-field">
-                      <label htmlFor="">Max. km stand</label>
-                      <input type="text" placeholder="vul een aantal in" />
-                    </div>
-                  </div>
-                </div>
-                <div className="form-third">
-                  <div className="input-field">
-                    <label htmlFor="">Aanvullende opmerkingen</label>
-                    <textarea name="" id=""
-                      placeholder="Eventuele extra wensen, belangrijke opties en/of opmerkingen kunnen hier ingevuld worden."></textarea>
-                  </div>
-                </div>
-                <div className="submit-btn">
-                  <button className="primary-btn arrow-btn grey-bg">Verzenden</button>
-                </div>
-              </div>
-              <div className="contact-right-box">
-                <div className="contact-right-inner orange-bg">
-                  <span className="title">contact opnemen</span>
-                  <ul>
-                    <li>
-                      <span>stuur een mail</span>
-                      <span>info@cartelier.nl</span>
-                    </li>
-                    <li>
-                      <span>bel ons</span>
-                      <span>+31 (0) 548 234880</span>
-                    </li>
-                    <li>
-                      <span>bezoek ons</span>
-                      <span>Galvanistraat 15E, 7461 JC Rijssen</span>
-                    </li>
-                  </ul>
-                </div>
-                <div className="contact-right-inner grey-bg">
-                  <span className="title">openingstijden</span>
-                  <ul>
-                    <li>
-                      <span>Maandag – vrijdag</span>
-                      <span>08:00 – 22:00</span>
-                    </li>
-                    <li>
-                      <span>zaterdag</span>
-                      <span>09:00 – 23:00</span>
-                    </li>
-                    <li>
-                      <span>Feestdagen</span>
-                      <span>10:00 – 20:00</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <section className="about-faq">
-        <FAQ />
-      </section>
+        </section>
+        <section className="werk-colums-section">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between werk-boxes">
+                    <div className="werk-box flex">
+                        <div className="werk-icon">
+                            <img src="/assets/icons/search.svg" alt="">
+                        </div>
+                        <div className="werk-content-box">
+                            <h3 className="ft-32 white-text">Inventarisatie van wensen</h3>
+                            <p>Wanneer u op zoek bent naar een specifieke auto, brengen wij uw wensen en eisen
+                                uitgebreid in kaart. Denk aan merk,
+                                model, uitvoering, budget en extra’s. Indien u een zoekopdracht heeft bespreken we
+                                binnen 1 week uw wensen zodat we
+                                direct kunnen starten met onze zoektocht voor u.</p>
+                        </div>
+                    </div>
+                    <div className="werk-box flex">
+                        <div className="werk-icon">
+                            <img src="/assets/icons/car-side.svg" alt="">
+                        </div>
+                        <div className="werk-content-box">
+                            <h3 className="ft-32 white-text">Gerichte inkoop</h3>
+                            <ul>
+                                <li>Bemiddeling: wij zoeken binnen ons netwerk naar auto’s die perfect aansluiten op uw
+                                    wensen.</li>
+                                <li>Eigen aanbod: wij kopen zelfstandig auto’s in die voldoen aan onze hoge
+                                    kwaliteitsstandaard en representatief zijn voor
+                                    ons aanbod. Belangrijk voor ons is dat de auto’s eerlijk, betrouwbaar en
+                                    dealeronderhouden zijn</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="werk-box flex">
+                        <div className="werk-icon">
+                            <img src="/assets/icons/screwdriver-wrench.svg" alt="">
+                        </div>
+                        <div className="werk-content-box">
+                            <h3 className="ft-32 white-text">Controle en voorbereiding</h3>
+                            <p>Elke auto – of deze nu in bemiddeling of via onze eigen inkoop wordt aangeboden –
+                                ondergaat een grondige technische en
+                                optische controle. Eventuele verbeteringen worden direct, naar wens, uitgevoerd zodat de
+                                auto in topstaat verkeert.</p>
+                        </div>
+                    </div>
+                    <div className="werk-box flex">
+                        <div className="werk-icon">
+                            <img src="/assets/icons/car-on.svg" alt="">
+                        </div>
+                        <div className="werk-content-box">
+                            <h3 className="ft-32 white-text">Presentatie en advies</h3>
+                            <p>Wij presenteren u de gevonden auto(’s) of tonen u direct ons actuele aanbod. U krijgt
+                                hierbij altijd eerlijk en
+                                deskundig advies.  </p>
+                        </div>
+                    </div>
+                    <div className="werk-box flex">
+                        <div className="werk-icon">
+                            <img src="/assets/icons/key.svg" alt="">
+                        </div>
+                        <div className="werk-content-box">
+                            <h3 className="ft-32 white-text">Levering en overdracht</h3>
+                            <p>De gekozen auto wordt rijklaar geleverd, inclusief documentatie en een duidelijke uitleg.
+                                U kunt rekenen op een
+                                zorgvuldige en persoonlijke overdracht. Indien de auto geimporteerd dient te worden
+                                regelen wij het importproces en BPM
+                                betaling. U zult hier niets van merken en wij ontzorgen hier volledig in.</p>
+                        </div>
+                    </div>
+                    <div className="werk-box flex">
+                        <div className="werk-icon">
+                            <img src="/assets/icons/handshake-angle.svg" alt="">
+                        </div>
+                        <div className="werk-content-box">
+                            <h3 className="ft-32 white-text">Nazorg en service</h3>
+                            <p>Ook na de aankoop blijft Cartelier BV uw aanspreekpunt. Of het nu gaat om advies,
+                                onderhoudstips of toekomstige wensen:
+                                wij staan voor u klaar!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </>
   )
 }
