b6b3f508eb0b1b0aa5e96fe5c55a85429b90adee
erjenweb <rijnderserjen@gmail.com>
2025-10-03 18:45:40 +0530

    Update faq.tsx

--- src/components/page/faq.tsx
+++ src/components/page/faq.tsx
@@ -46,22 +46,16 @@ function FAQ() {
             >
               {faq.question}
               {/* Simple Chevron SVG (rotates on toggle) */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={`h-5 w-5 transition-transform duration-300 ${
+              <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg"
+                lassName={`h-5 w-5 transition-transform duration-300 ${
                   openIndex === i ? "rotate-180" : ""
-                }`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M19 9l-7 7-7-7"
-                />
+                }`}>
+              <path d="M0.957031 14.7609C0.957031 8.53197 6.00658 3.48242 12.2355 3.48242H18.7652C24.9941 3.48242 30.0436 8.53197 30.0436 14.7609C30.0436 20.9898 24.9941 26.0394 18.7651 26.0394H12.2355C6.00657 26.0394 0.957031 20.9898 0.957031 14.7609Z" fill="#708585"/>
+              <rect x="0.5" width="30" height="30" rx="15" fill="#708585"/>
+              <path d="M14.6875 21.5586L10.0938 16.9648L9.62891 16.5L10.5586 15.5703L11.0234 16.0352L14.4961 19.5078V7.75H15.8086V19.5078L19.2812 16.0352L19.7461 15.5703L20.6758 16.5L20.2109 16.9648L15.6172 21.5586L15.1523 22.0234L14.6875 21.5586Z" fill="#132025"/>
               </svg>
+
+             
             </button>
 
             {/* Collapsible Answer */}
