9347f6477b97d9be08b803825569b4a2175395e6
erjenweb <rijnderserjen@gmail.com>
2025-10-04 13:38:11 +0530

    Update brand.tsx

--- src/components/home/brand.tsx
+++ src/components/home/brand.tsx
@@ -44,25 +44,42 @@ function Brand() {
 
           <SwiperSlide className='swiper-slide'>
             <div className="brand-logo flex items-center justify-center">
-              <Image src="/assets/icons/cupra.svg" alt="Cupra" width={100} height={100} />
+              <Image src="/assets/icons/porsche-svgrepo-com.svg" alt="Porche" width={100} height={100} />
             </div>
           </SwiperSlide>
 
           <SwiperSlide className='swiper-slide'>
             <div className="brand-logo flex items-center justify-center">
-              <Image src="/assets/icons/star.svg" alt="Star" width={100} height={100} />
+              <Image src="/assets/icons/audi-new-logo.svg" alt="audi" width={100} height={100} />
             </div>
           </SwiperSlide>
 
           <SwiperSlide className='swiper-slide'>
             <div className="brand-logo flex items-center justify-center">
-              <Image src="/assets/icons/mg.svg" alt="MG" width={100} height={100} />
+              <Image src="/assets/icons/bmw-7.svg" alt="BMW" width={100} height={100} />
             </div>
           </SwiperSlide>
 
           <SwiperSlide className='swiper-slide'>
             <div className="brand-logo flex items-center justify-center">
-              <Image src="/assets/icons/renolt.svg" alt="Renault" width={100} height={100} />
+              <Image src="/assets/icons/volvo.svg" alt="Volvo" width={100} height={100} />
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide className='swiper-slide'>
+            <div className="brand-logo flex items-center justify-center">
+              <Image src="/assets/icons/volkswagen-10.svg" alt="Volkswagen" width={100} height={100} />
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide className='swiper-slide'>
+            <div className="brand-logo flex items-center justify-center">
+              <Image src="/assets/icons/bmw-mini-1.svg" alt="Mini Cooper" width={100} height={100} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className='swiper-slide'>
+            <div className="brand-logo flex items-center justify-center">
+              <Image src="/assets/icons/lynk-logo.png" alt="Lynk" width={100} height={100} />
             </div>
           </SwiperSlide>
 
